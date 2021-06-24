e3d28f0e3692342dcaea58e9571986118ae5f7d7
Dennise Chávez <80653433+dennisechavez@users.noreply.github.com>
2021-06-24 13:37:36 -0400

    Added content and updated UI to match designs.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
+function toggleNav() {
+  var x = document.getElementById("navLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
